1d7a1a14d5551072032ed7ea3871e62fac142119
izhanshahid-cloud <izhanshahid123@gmail.com>
2026-05-10 04:22:49 +0500

    Update Assignment1.java

--- Assignment1.java
+++ Assignment1.java
@@ -1,4 +1,4 @@
-public class Assign__1 {
+public class Assign1 {
         public static void main(String[] args) {
                 java.util.Scanner sc = new java.util.Scanner(System.in);
                 System.out.print("Enter number of rows: ");
